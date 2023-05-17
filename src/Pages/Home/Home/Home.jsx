f2879715cd3ddf77505649a2f35e01cf49ce8b07
debabrata2003 <debabratacmt@gmail.com>
2023-05-17 18:20:07 +0600

    secod

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,16 @@
+import About from "../About/About";
+import Banner from "../Banner/Banner";
+import Service from "../Service/Service";
+
+const Home = () => {
+    return (
+        <div>
+            <Banner></Banner>
+            <About></About>
+            <Service></Service>
+            <h2>This is Home</h2>
+        </div>
+    );
+};
+
+export default Home;
